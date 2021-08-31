900f5f8bdc75b86a8b9b4401b8f1bd9a84d5ea6c
Andrey Buzunov <a.buzunov@audiotele.ru>
2021-08-31 10:07:44 +0400

    1st com

---
+++ tests/test1.py
@@ -0,0 +1,22 @@
+#print('Welcome, my test programm, enter your name')
+#name = input()
+#print('Hello,', name)
+#phone = input('Enter your phone number ')
+#print('Greate,', name, 'your phone number is', phone)
+#n = int(input())
+#print((n + 3) // 4)
+#x = int(input())
+#a = x // 1000
+#b = x // 100 % 10
+#c = x % 100 // 10
+#d = x % 10
+#print(a, b, c, d)
+a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+
+    if b1 > a2:
+       print(a2, b1)
+    elif b1 < a2:
+        print('пустое множество')
+    elif b1 == a2:
+        print(a2)
+
